d824f7c3f1c1a800a94828de48b32c8a94d3d862
João Alberto <jancunha@gmail.com>
2023-03-27 22:50:38 -0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "regal-sun-343102"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "credentials" {
+  default = ".keys/gcp.json"
+}
